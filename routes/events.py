617657b6e1955b833a4e5ef247c44a1ceaa1a5f8
Patrick Guinn <151584815+PG-23@users.noreply.github.com>
2025-03-05 22:18:12 -0600

    Update events.py

---
+++ routes/events.py
@@ -0,0 +1,9 @@
+# routes/events.py
+from flask import Blueprint
+
+events_bp = Blueprint('events', __name__)
+
+# Your route definitions here
+# @events_bp.route('/events', methods=['GET'])
+# def get_events():
+#     ...
