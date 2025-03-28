6ea6652bd822f5b6f0376c378c4347fbfda7524e
PG-23 <patrick_guinn@yahoo.com>
2025-03-28 09:21:31 -0500

    Added detailed leveling system

--- routes/__init__.py
+++ routes/__init__.py
@@ -5,3 +5,6 @@ from .rewards import rewards_bp
 
 # __all__ is a special variable in Python that specifies what symbols to export
 __all__ = ['events_bp', 'leveling_bp', 'rewards_bp']
+
+# __all__ is a special variable in Python that specifies what symbols to export
+__all__ = ['events_bp', 'leveling_bp', 'rewards_bp']
