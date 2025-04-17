e79bfac20e62cc565bd34b6f9d31031455e5fd85
saineethubonagiri <157181471+saineethubonagiri@users.noreply.github.com>
2025-04-17 11:56:44 -0500

    Made changes

---
+++ migrations/versions/966caab35eb0_merging_heads.py
@@ -0,0 +1,24 @@
+"""Merging heads
+
+Revision ID: 966caab35eb0
+Revises: 4c6dae6e9c2c, fdaf0f99ee5c
+Create Date: 2025-04-14 14:34:17.116602
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '966caab35eb0'
+down_revision = ('4c6dae6e9c2c', 'fdaf0f99ee5c')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
